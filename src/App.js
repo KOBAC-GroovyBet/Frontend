1a598b910ffe3d9e01a1e8eea24c2e7092799683
Ruthgyeul <wingd4705@gmail.com>
2025-01-01 18:03:24 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+// src/App.js
+import AppRoutes from './routes/Routes';
+
+function App() {
+  return <AppRoutes />;
+}
+
+export default App;
---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,18 @@
+// src/layouts/MainLayout.jsx
+import { Outlet } from 'react-router-dom';
+import { Header } from '../components/common/Header';
+import { Footer } from '../components/common/Footer';
+
+const MainLayout = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayout;
